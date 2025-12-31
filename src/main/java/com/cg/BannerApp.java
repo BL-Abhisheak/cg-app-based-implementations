448b7795e9cb9cb274d121178ab0d0e194f7b0c1
BL-Abhisheak <abhisheak.bhaskaran@bridgelabz.com>
2025-12-31 11:27:56 +0530

    UC3: Refactor Banner Logic into Functions

--- src/main/java/com/cg/BannerApp.java
+++ src/main/java/com/cg/BannerApp.java
@@ -1,41 +1,64 @@
 package com.cg;
 
 public class BannerApp {
+
+    public static String[] printO(){
+     return new String[]{
+             "   ***    ",
+             " **   **  ",
+             "**     ** ",
+             "**     ** ",
+             "**     ** ",
+             "**     ** ",
+             "**     ** ",
+             " **   **  ",
+             "   ***    "
+     };
+
+    }
+
+
+
+    public static String[] printP(){
+        return new String[]{
+                "******    ",
+                "**    **  ",
+                "**     ** ",
+                "**    **  ",
+                "******    ",
+                "**        ",
+                "**        ",
+                "**        ",
+                "**        "
+        };
+
+    }
+
+
+    public static String[] printS(){
+        return new String[]{
+                "   ***** ",
+                " **      ",
+                "**       ",
+                " **      ",
+                "   ***   ",
+                "      ** ",
+                "       **",
+                "      ** ",
+                " *****   "
+        };
+
+    }
+
     public static void main(String[] args) {
         System.out.println("oops");
 
-        String[] lineDisplay = {
-                "   ***       ***    ******       *****",
+        String[] oPattern = printO();
+        String[] pPattern = printP();
+        String[] sPattern = printS();
 
-
-
-                " **   **   **   **  **    **   **",
-
-
-                "**     ** **     ** **     ** **",
-
-
-                "**     ** **     ** **    **   **",
-
-
-                "**     ** **     ** ******       ***",
-
-
-                "**     ** **     ** **              **",
-
-
-                "**     ** **     ** **               **",
-
-
-                " **   **   **   **  **              **",
-
-
-                "   ***       ***    **         *****"
-        };
-
-
-        for (String line : lineDisplay){
-            System.out.println(line);
+        for(int i = 0; i < oPattern.length; i++){
+            System.out.println(oPattern[i] + oPattern[i] + pPattern[i] + sPattern[i]);
         }
     }
 }
